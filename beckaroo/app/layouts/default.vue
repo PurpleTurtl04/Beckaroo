bf7e0c1855fc44350eb57cc490511b846d72de4a
Nick Edwards <nledwa01@gmail.com>
2026-03-29 15:10:40 -0500

    Created Nuxt 4 application. Added modules and plugins. NuxtESLint, NuxtFonts,NuxtTailwindcss, NuxtImage, NuxtTailwindcss, DiasyUI, Prettier, Prettier Tailwind Plugin, ESLint Config Prettier

---
+++ beckaroo/app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <div class="flex min-h-screen flex-col">
+            <main class="container mx-auto grow py-6">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
---
+++ beckaroo/app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="mt-2 text-3xl">Beckaroo & The Zoo</h1>
+    </div>
+</template>
