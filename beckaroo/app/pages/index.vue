17d05c9157f835cbb09fb17bc47cd47a4aaaa5d8
Nick Edwards <nledwa01@gmail.com>
2026-03-29 22:12:47 -0500

    Installed daisyUI. Updated nuxt config fil to use css file and added app section with htmlAttrs.

--- beckaroo/app/pages/index.vue
+++ beckaroo/app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <h1 class="mt-2 text-3xl">Beckaroo & The Zoo</h1>
+        <div class="btn btn-primary">Test</div>
     </div>
 </template>
--- beckaroo/nuxt.config.ts
+++ beckaroo/nuxt.config.ts
@@ -1,12 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+    modules: [
+        '@nuxt/eslint',
+        '@nuxt/fonts',
+        '@nuxt/image',
+        '@nuxtjs/tailwindcss',
+    ],
+    css: ['~/assets/css/main.css'],
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'en',
+                'data-theme': 'emerald',
+            },
+            meta: [
+                { charset: 'utf-8' },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+            ],
+            title: 'Beckaroo & The Zoo',
+        },
+    },
+});
